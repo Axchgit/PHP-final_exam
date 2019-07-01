cdbebec524be2aaad003e74e94b946f14d490794
Axchtzon <axchtzon@dingtalk.com>
2019-07-01 11:07:16 +0800

    修改蔬菜图片

--- amend_vege.php
+++ amend_vege.php
@@ -55,13 +55,13 @@
 				
 		</span>
 	</p>
-	<!--<p>
+	<p>
 		<span class="sp1">图片：
 		</span>
 	    <span class="sp2">
 			<input class=""  name="myfile" type="file"   >
 	    </span>
-	</p>-->
+	</p>
 	<p>
 		<span class="sp3">
 			<input name="add" class="bt1" type="submit" value="提交">
@@ -83,19 +83,19 @@ if(isset($_POST['add'])){
 		$p=$_POST['pur'];
 		$s=$_POST['sell'];
 		$dt=date("Y-m-d H:i:s",time());
-//		$arr=$_FILES['myfile'];
-//		$arr['tmp_name'];
-//		$filename="picture/".$arr['name'];
-//		move_uploaded_file($arr['tmp_name'],$filename);
+		$arr=$_FILES['myfile'];
+		$arr['tmp_name'];
+		$filename="picture/".$arr['name'];
+		move_uploaded_file($arr['tmp_name'],$filename);
 		$id=$_GET['id'];
 	
-	$sql1="update vegetable_message set ori_place=:c,pur_price=:d,sell_price=:e,add_time=:f where id=$id";
+	$sql1="update vegetable_message set picture=:a, ori_place=:c,pur_price=:d,sell_price=:e,add_time=:f where id=$id";
 //	$sql2="select run_id from users_message group by run_id having count(*)>1";
 	
 //	$pdo->beginTransaction();
 	$ps = $pdo->prepare($sql1);
 	
-//	$ps->bindParam("a",$filename);
+	$ps->bindParam("a",$filename);
 //	$ps->bindParam("b",$n);
 	$ps->bindParam("c",$o);
 	$ps->bindParam("d",$p);
@@ -107,44 +107,6 @@ if(isset($_POST['add'])){
 	}else{
 		echo  "<script>alert('修改失败');document.location.href='vege_message.php';</script>";
 	}
-//	if(!$ps->execute()){
-//		print_r($ps->errorInfo());
-//	}
-//	$re=$pdo->query($sql2);
-//	if($re->rowCount()>0){
-//		$pdo->rollback();
-//		echo  "<script>alert('获取随机ID号已存在，请重新获取');document.location.href='user_add.php';</script>";
-//	}else{
-//		$pdo->commit();
-//		echo "注册成功，您的ID号为：".$run_id."    此为登陆凭证，请妥善保管";
-//		echo '<div style="text-align: center;"><a  href="user_login.php"><button  class="bt2">确定</button></a></div>';
-//		if(isset($_POST['con'])){
-//			header("location:user_login.php");
-//			
-//		}
-		
-		
-//		echo "<script>alert('注册成功');document.location.href='user_login.php';</script>";
-	
-	
-	
-//	$sql2="select a.* from admin_message a,(select name,password from admin_message group by name,password having count(*)>1) b where a.name=b.name and a.password=b.password";
-//	$sql3="DELETE from admin_message
-//order by id DESC limit 1";
-//	
-//	$pdo->query($sql1);
-//	$r=$pdo->query($sql2);
-
-//	if($r->rowCount()>0){
-//			$pdo->query($sql3);
-//echo "<script>alert('该账户信息已存在');document.location.href='admin_add.php';</script>";
-//	}else{
-//		echo "<script>alert('注册成功');</script>";
-//		header('location:admin_login.php');
-//		echo "<script>alert('注册成功');document.location.href='';</script>";
-//	}
-	
-	//$link->close();
 
 
 
